de027e9367c2ad69aff6ed1a4cd2d78484bfcced
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 18:03:20 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -15,9 +15,11 @@ def third_element(array)
 end
 
 def last_element(array)
-  array[array.count]
+  array[array.count - 1]
 end
 
 def first_element_with_array_methods(array)
   array.first
 end
+
+def 
