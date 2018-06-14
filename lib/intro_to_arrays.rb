8e146704b49866b3a840a71b189c87d95325888c
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 18:04:08 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,4 +22,10 @@ def first_element_with_array_methods(array)
   array.first
 end
 
-def 
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.count
+end
